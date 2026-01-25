b998c27bc30d2f787e89346de6455d64b472e349
MOSES <furlandave@gmail.com>
2026-01-25 23:30:46 +0100

    its harder than i thought Prod/Cons problem

--- 3_producer_consumer/producer_consumer.cpp
+++ 3_producer_consumer/producer_consumer.cpp
@@ -1,18 +1,35 @@
 #include <omp.h>
 #include <stdio.h>
 #include <iostream>
-
+#include <mutex>
 using namespace std; 
 
 // TODO i need a MUTEX
+mutex buff_mutex;
 
-void producer(){
+
+int available_items(int [] arr){
+	if(arr[0]==NULL)
+		return 0;
+	if(arr[-1]!=NULL)
+		return arr.length-1;
+
+	for(int i=0;i<arr.length;i++){
+		if(arr[i]==NULL)
+			return i+1;
+	}
+}
+
+void producer(int writer_id, int [] arr){
 	// TODO when to be activated? when available_items == 0
-	
+	if()	
 }
 
 void consumer(){
 	// TODO when to be activated? when available_items >= 0
+	if(available_items(buff)==0);
+		//call producer
+	
 }
 
 int main()
